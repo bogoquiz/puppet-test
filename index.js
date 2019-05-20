356c5730d2aab371ac593c494338fc76970fe5af
memen <contactsaliens@gmail.com>
2019-05-19 20:37:48 -0500

    async

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end('<h1>Hello, World!</h1>')
+  pepper()
 })
 
 server.listen(port, () => {
   console.log(`Server running at port ${port}`)
-  (async () => {
-    const browser = await puppeteer.launch({headless:true, sandbox:false, args : [
-          '--remote-debugging-port=9222','--disable-setuid-sandbox', '--no-sandbox'
-      ]});
-    const page = await browser.newPage();
-    await page.goto('https://futbol-twit.herokuapp.com/');
-    page.on('console', msg => {
-      console.log('hola')
-    //for (let i = 0; i < msg.args().length; i++) {
-    //  console.log(msg.args()[i]);
-    //	}
-    });
-    //await browser.close();
-  })();
+  
 })
+async function pepper() {
+  const browser = await puppeteer.launch({headless:true, sandbox:false, args : [
+    '--remote-debugging-port=9222','--disable-setuid-sandbox', '--no-sandbox'
+  ]});
+  const page = await browser.newPage();
+  await page.goto('https://futbol-twit.herokuapp.com/');
+  page.on('console', msg => {
+  console.log('hola')
+  //for (let i = 0; i < msg.args().length; i++) {
+  //  console.log(msg.args()[i]);
+  //	}
+  });
+  //await browser.close();
+}
